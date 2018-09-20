a600da5cc63bda781cbde486d34e54de7bed57ef
aulia <aulia7717@gmail.com>
2018-09-20 14:24:52 +0700

    prolog aul

--- app/src/main/java/com/example/aflah/tracki_master/SplashScreen.java
+++ app/src/main/java/com/example/aflah/tracki_master/SplashScreen.java
@@ -15,7 +15,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, NavigationActivity.class));
+                startActivity(new Intent(SplashScreen.this, PrologActivity.class));
                 finish();
             }
         }, 2000);
