93b656abaf2919efaf662d57388ecf6fc1397391
Dino Puguh <puguhmunung16@gmail.com>
2018-09-20 20:58:56 +0700

    detail toko

--- app/src/main/java/com/example/aflah/tracki_master/HomeFragment.java
+++ app/src/main/java/com/example/aflah/tracki_master/HomeFragment.java
@@ -1,12 +1,18 @@
 package com.example.aflah.tracki_master;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.aflah.tracki_master.View.Toko.DetailTokoActivity;
 
 
 /**
@@ -26,6 +32,8 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button btnDetailToko;
 
     private OnFragmentInteractionListener mListener;
 
@@ -63,8 +71,25 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnDetailToko = (Button)view.findViewById(R.id.btnDetailToko);
+
+        btnDetailToko.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewDetailToko = new Intent(getActivity(), DetailTokoActivity.class);
+                getActivity().startActivity(viewDetailToko);
+            }
+        });
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/aflah/tracki_master/View/Toko/DetailTokoActivity.java
+++ app/src/main/java/com/example/aflah/tracki_master/View/Toko/DetailTokoActivity.java
@@ -3,6 +3,8 @@ package com.example.aflah.tracki_master.View.Toko;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 
 import com.example.aflah.tracki_master.R;
 
@@ -11,5 +13,8 @@ public class DetailTokoActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_toko);
+
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.favourite);
+
     }
 }
