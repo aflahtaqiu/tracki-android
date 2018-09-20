6220413698f9dbd3197684a87dfe3da9f820515b
Dino Puguh <puguhmunung16@gmail.com>
2018-09-20 09:53:27 +0700

    create detail toko

--- app/src/main/java/com/example/aflah/tracki_master/View/Toko/DetailTokoActivity.java
+++ app/src/main/java/com/example/aflah/tracki_master/View/Toko/DetailTokoActivity.java
@@ -1,7 +1,15 @@
 package com.example.aflah.tracki_master.View.Toko;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class DetailTokoActivity extends AppCompatActivity{
+import com.example.aflah.tracki_master.R;
 
+public class DetailTokoActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_toko);
+    }
 }
