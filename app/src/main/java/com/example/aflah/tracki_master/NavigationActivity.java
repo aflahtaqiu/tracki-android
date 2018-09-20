3ac8b64cc3f3c64d5412a976312ce95b6e35da64
aflahtaqiu <aflahts98@gmail.com>
2018-09-20 22:21:43 +0700

    refactoring navbar

--- app/src/main/java/com/example/aflah/tracki_master/NavigationActivity.java
+++ app/src/main/java/com/example/aflah/tracki_master/NavigationActivity.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class NavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, HomeFragment.OnFragmentInteractionListener, MapFragment.OnFragmentInteractionListener, AccountFragment.OnFragmentInteractionListener {
+
+    private HomeFragment homeFragment = null;
+    private MapFragment mapFragment = null;
+    private AccountFragment accountFragment = null;
+    private FragmentTransaction fragmentTransaction;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,21 +26,27 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     setTitle("Home Fragment");
-                    HomeFragment homeFragment = new HomeFragment();
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.content, homeFragment, "Home Fragment").commit();
+                    if (homeFragment == null){
+                        homeFragment = new HomeFragment();
+                    }
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, homeFragment, "").commit();
                     return true;
                 case R.id.navigation_map:
                     setTitle("Map Fragment");
-                    MapFragment mapFragment = new MapFragment();
-                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction1.replace(R.id.content, mapFragment, "Map Fragment").commit();
+                    if (mapFragment == null){
+                        mapFragment = new MapFragment();
+                    }
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, mapFragment, "").commit();
                     return true;
                 case R.id.navigation_account:
                     setTitle("Account Fragment");
-                    AccountFragment accountFragment = new AccountFragment();
-                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction2.replace(R.id.content, accountFragment, "Account Fragment").commit();
+                    if (accountFragment == null){
+                        accountFragment = new AccountFragment();
+                    }
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, accountFragment, "").commit();
                     return true;
             }
             return false;
@@ -49,10 +61,9 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        setTitle("Home Fragment");
-        HomeFragment homeFragment = new HomeFragment();
-        FragmentTransaction fragmentTransactionInit = getSupportFragmentManager().beginTransaction();
-        fragmentTransactionInit.replace(R.id.content, homeFragment, "Home Fragment").commit();
+        homeFragment = new HomeFragment();
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, homeFragment, "").commit();
     }
 
     @Override
