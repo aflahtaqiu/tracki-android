233afc2b9d1349620fead43d1330988181f7cdfa
Ennobel <ennobell17@gmail.com>
2018-10-02 09:33:26 +0700

    terakhir

--- app/src/main/java/com/example/aflah/tracki_master/NavbarFragment/HomeFragment.java
+++ app/src/main/java/com/example/aflah/tracki_master/NavbarFragment/HomeFragment.java
@@ -1,13 +1,17 @@
 package com.example.aflah.tracki_master.NavbarFragment;
 
+import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,12 +20,28 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.example.aflah.tracki_master.View.Toko.DetailTokoActivity;
 
 import com.example.aflah.tracki_master.NavigationActivity;
 import com.example.aflah.tracki_master.R;
+import com.eyro.cubeacon.CBBeacon;
+import com.eyro.cubeacon.CBRangingListener;
+import com.eyro.cubeacon.CBRegion;
+import com.eyro.cubeacon.CBServiceListener;
+import com.eyro.cubeacon.Cubeacon;
+import com.eyro.cubeacon.SystemRequirementManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
 
 
 /**
@@ -32,11 +52,17 @@ import com.example.aflah.tracki_master.R;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements CBRangingListener, CBServiceListener,AdapterView.OnItemClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = HomeFragment.class.getSimpleName();
+    private Cubeacon cubeacon;
+    private ListView listView;
+    private SimpleAdapter adapter;
+    private List<Map<String, String>> data;
+    private  List<CBBeacon> beacons;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -54,7 +80,6 @@ public class HomeFragment extends Fragment {
     public HomeFragment() {
         // Required empty public constructor
     }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -76,11 +101,13 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Cubeacon.initialize(getContext());
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
         setHasOptionsMenu(true);
+
     }
 
     @Override
@@ -98,9 +125,24 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        listView = (ListView) view.findViewById(R.id.listview);
+        String[] from = new String[]{"title", "subtitle"};
+        int[] to = new int[]{android.R.id.text1, android.R.id.text2};
+        data = new ArrayList<>();
+        adapter = new SimpleAdapter(getContext(), data, android.R.layout.simple_list_item_2, from, to);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                CBBeacon beacon = beacons.get(i);
+                Intent intent = new Intent(getActivity(), MonitoringActivity.class);
+                intent.putExtra(MonitoringActivity.INTENT_BEACON, beacon);
+                startActivity(intent);
+            }
+        });
+        cubeacon = Cubeacon.getInstance();
 
         btnDetailToko = (Button) view.findViewById(R.id.btnDetailToko);
-
         btnDetailToko.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -108,6 +150,27 @@ public class HomeFragment extends Fragment {
                 getActivity().startActivity(viewDetailToko);
             }
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // check all requirement like is BLE available, is bluetooth on/off,
+        // location service for Android API 23 or later
+        if (SystemRequirementManager.checkAllRequirementUsingDefaultDialog((Activity) getContext())) {
+            // connecting to Cubeacon service when all requirements completed
+            cubeacon.connect(this);
+            // disable background mode, because we're going to use full
+            // scanning resource in foreground mode
+            cubeacon.setBackgroundMode(false);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cubeacon.disconnect(this);
     }
 
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -170,6 +233,71 @@ public class HomeFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(List<CBBeacon> list, CBRegion cbRegion) {
+        this.beacons = beacons;
+
+        String title, subtitle;
+        Map<String, String> map;
+
+        // clear data
+        data.clear();
+        for (CBBeacon beacon : beacons) {
+            title = beacon.getProximityUUID().toString().toUpperCase();
+            subtitle = String.format(Locale.getDefault(), "M:%d m:%d RSSI:%d Accuracy:%.2fm",
+                    beacon.getMajor(), beacon.getMinor(), beacon.getRssi(), beacon.getAccuracy());
+            map = new HashMap<>();
+            map.put("title", title);
+            map.put("subtitle", subtitle);
+            data.add(map);
+        }
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                adapter.notifyDataSetChanged();
+                if (((AppCompatActivity)getActivity()).getSupportActionBar() != null) {
+                    ((AppCompatActivity)getActivity()).getSupportActionBar().setSubtitle("Ranged beacon : " + beacons.size());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        // add ranging listener implementation
+        cubeacon.addRangingListener(this);
+        try {
+            // create a new region for ranging beacons
+            CBRegion region = new CBRegion("com.eyro.cubeacon.ranging_region",
+                    UUID.fromString("CB10023F-A318-3394-4199-A8730C7C1AEC"));
+            // start ranging beacons using region
+            cubeacon.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error while start ranging beacon, " + e);
+        }
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        return null;
+    }
+
+    @Override
+    public void unbindService(ServiceConnection serviceConnection) {
+
+    }
+
+    @Override
+    public boolean bindService(Intent intent, ServiceConnection serviceConnection, int i) {
+        return false;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 
     /**
