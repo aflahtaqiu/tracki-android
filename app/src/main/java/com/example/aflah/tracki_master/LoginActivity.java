3fb0823c7a1668ac0ef39cd025e7a2449617e63e
farkhandj <farkhanjanaid@gmail.com>
2018-10-18 16:14:59 +0700

    loginku farkhan

--- app/src/main/java/com/example/aflah/tracki_master/LoginActivity.java
+++ app/src/main/java/com/example/aflah/tracki_master/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.example.aflah.tracki_master;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
     }
 }
