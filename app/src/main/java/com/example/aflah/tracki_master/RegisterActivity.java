e26dabe8fd85efc50dd4c8431895c22718d3cf7a
shania <shania.tanti6@gmail.com>
2018-10-20 20:29:36 +0700

    commit

--- app/src/main/java/com/example/aflah/tracki_master/RegisterActivity.java
+++ app/src/main/java/com/example/aflah/tracki_master/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.aflah.tracki_master;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    public void onStart() {
+        super.onStart();
+
+        EditText txtDate = (EditText) findViewById(R.id.txtdate);
+        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View view, boolean hasfocus) {
+                if (hasfocus) {
+                    DateDialog dialog = new DateDialog(view);
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    dialog.show(ft, "DatePicker");
+
+                }
+            }
+
+        });
     }
 }
