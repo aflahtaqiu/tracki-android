328770ff08f75e441bd2dab1ef206994fda11c95
aflahtaqiu <aflahts98@gmail.com>
2018-10-30 08:56:38 +0700

    tess

--- app/src/main/java/com/example/aflah/tracki_master/Model/Response/ResponseLogin.java
+++ app/src/main/java/com/example/aflah/tracki_master/Model/Response/ResponseLogin.java
@@ -1,5 +1,6 @@
 package com.example.aflah.tracki_master.Model.Response;
 
+import com.example.aflah.tracki_master.Model.UserLogin;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -8,5 +9,55 @@ public class ResponseLogin {
     @SerializedName("message")
     @Expose
     private String message;
+    @SerializedName("user")
+    @Expose
+    private UserLogin userLogin;
+    @SerializedName("access_token")
+    @Expose
+    private String accessToken;
+    @SerializedName("token_type")
+    @Expose
+    private String tokenType;
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UserLogin getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(UserLogin userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseLogin{" +
+                "message='" + message + '\'' +
+                ", userLogin=" + userLogin +
+                ", accessToken='" + accessToken + '\'' +
+                ", tokenType='" + tokenType + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/example/aflah/tracki_master/Model/UserLogin.java
+++ app/src/main/java/com/example/aflah/tracki_master/Model/UserLogin.java
@@ -1,4 +1,110 @@
 package com.example.aflah.tracki_master.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class UserLogin {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("active")
+    @Expose
+    private boolean active;
+    @SerializedName("date_of_birth")
+    @Expose
+    private String dateOfBirth;
+    @SerializedName("foto")
+    @Expose
+    private String foto;
+    @SerializedName("provider")
+    @Expose
+    private String provider;
+    @SerializedName("provider_id")
+    @Expose
+    private String providerId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLogin{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", active=" + active +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", foto='" + foto + '\'' +
+                ", provider='" + provider + '\'' +
+                ", providerId='" + providerId + '\'' +
+                '}';
+    }
 }
