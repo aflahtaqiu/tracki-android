579cdcab9a6853b4e418b7d39e05815ba16f8dce
aulia <aulia7717@gmail.com>
2018-10-30 11:35:42 +0700

    aul

--- app/src/main/java/com/example/aflah/tracki_master/AboutAppActivity.java
+++ app/src/main/java/com/example/aflah/tracki_master/AboutAppActivity.java
@@ -3,11 +3,11 @@ package com.example.aflah.tracki_master;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class setting extends AppCompatActivity {
+public class AboutAppActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_about_app);
     }
 }
