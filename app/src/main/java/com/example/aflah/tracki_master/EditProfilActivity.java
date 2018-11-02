bb22a701c0aaf8b3e84d7e54de9008990dd7c30f
shania <shania.tanti6@gmail.com>
2018-11-02 10:23:27 +0700

    commit

--- app/src/main/java/com/example/aflah/tracki_master/EditProfilActivity.java
+++ app/src/main/java/com/example/aflah/tracki_master/EditProfilActivity.java
@@ -1,26 +1,55 @@
 package com.example.aflah.tracki_master;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.PersistableBundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 
 public class EditProfilActivity extends Activity{
 
-    private ImageView profile;
+    ImageView show;
+    TextView show1;
+    Dialog Mydialog;
+    TextView picAvatar;
+    TextView picGaleri;
+    private int GALLERY = 1;
+    private static final String IMAGE_DIRECTORY = "/Tracki";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profil);
 
-
+        show = (ImageView)findViewById(R.id.profile);
+        show1 = (TextView)findViewById(R.id.gantiFoto);
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyCustomDialog();
+            }
+        });
     }
     public void onStart() {
         super.onStart();
@@ -35,6 +64,86 @@ public class EditProfilActivity extends Activity{
                 }
             }
         });
+    }
+
+    public void MyCustomDialog(){
+        Mydialog = new Dialog(EditProfilActivity.this);
+        Mydialog.setContentView(R.layout.popup_normal);
+
+        picAvatar = (TextView)Mydialog.findViewById(R.id.imgAvatar);
+        picGaleri = (TextView)Mydialog.findViewById(R.id.imgGaleri);
+
+        picAvatar.setEnabled(true);
+        picGaleri.setEnabled(true);
+
+        picAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Pilih default avatar", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        picGaleri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, GALLERY);
+            }
+        });
+        Mydialog.show();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    String path = saveImage(bitmap);
+                    Toast.makeText(EditProfilActivity.this, "Gambar tersimpan", Toast.LENGTH_SHORT).show();
+                    show.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(EditProfilActivity.this, "Gambar gagal tersimpan", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+    }
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
     }
 
 }
