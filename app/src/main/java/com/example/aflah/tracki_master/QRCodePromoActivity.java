d4720d25fa8a3936be0be4c2efe71187750ac415
aflahtaqiu <aflahts98@gmail.com>
2018-11-27 00:07:26 +0700

    generate qrcode

--- app/src/main/java/com/example/aflah/tracki_master/QRCodePromoActivity.java
+++ app/src/main/java/com/example/aflah/tracki_master/QRCodePromoActivity.java
@@ -1,11 +1,23 @@
 package com.example.aflah.tracki_master;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 public class QRCodePromoActivity extends AppCompatActivity {
+
+    ImageView imageViewQRCode;
+    String generateQRCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +28,20 @@ public class QRCodePromoActivity extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
         getWindow().setLayout((int)(displayMetrics.widthPixels*.8), (int)(displayMetrics.heightPixels*.6));
+
+        imageViewQRCode = (ImageView) findViewById(R.id.qrCode_generatePromo);
+        generateQRCode = getIntent().getExtras().getString("qrCodeString");
+        Log.v("qrCodeString", " "+generateQRCode);
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(generateQRCode, BarcodeFormat.QR_CODE,250, 250 );
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageViewQRCode.setImageBitmap(bitmap);
+        }catch (WriterException e){
+            e.printStackTrace();
+        }
 
     }
 }
